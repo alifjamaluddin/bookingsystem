24a7242f6171a01c810ac31e75f328302e88ca91
alifjamaluddin <alifjamaluddin92@gmail.com>
2015-06-21 02:52:48 +0800

    Add landing page for admin

---
+++ template/admin-menu.php
@@ -0,0 +1,24 @@
+	<nav class="menu" id="menu">
+		<div class="menu-scroll">
+			<div class="menu-wrap">
+				<div class="menu-content">
+					<a class="menu-logo" data-toggle="menu" href="#menu">Menu</a>
+					<ul class="nav">
+						<li>
+							<a href="admin-calendar.php">Calendar</a>
+						</li>
+						<li>
+							<a href="admin-reservation.php">Reservation</a>
+						</li>
+						<li>
+							<a href="admin-registration.php">Registration</a>
+						</li>
+						<li>
+							<a href="admin-report.php">Report</a>
+						</li>
+					</ul>
+
+				</div>
+			</div>
+		</div>
+	</nav>
---
+++ template/frontpage/admin-about.php
@@ -0,0 +1,12 @@
+<div class="row about-header">
+	<h2>STUDENT ACTIVITY CENTER UITM JASIN</h2>	
+</div>
+<div class="col-lg-6 col-lg-push-3 col-sm-10 col-sm-push-1">
+	<div class="about-img">
+	<img src="../images/samples/landscape.jpg">
+	</div>
+	<blockquote>
+		<p>Student Activity Center located in UiTM Jasin is provided for student to do activities. Student Activity Center has 2
+			unit department which are Sport Unit and Co-curiculum Unit. It consists of 6 Bilik Persatuan, 1 Majlis Perwakilan Pelajar (MPP) Conference Room, 1 pingpong court</p>
+		</blockquote>
+	</div>
